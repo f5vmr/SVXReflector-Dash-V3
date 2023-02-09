f32bdb6e84e098437ed17ae5870f3f3accfe1889
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-02-09 16:17:01 +0000

    n

--- access.php
+++ access.php
@@ -4,8 +4,10 @@
 
   // Get the existing username and password information from the Raspberry Pi
   $existing_username = exec("echo $USER");
+  echo $existing_username;
   $existing_password = exec("cat /etc/shadow | grep $existing_username | awk -F':' '{print $2}'");
-
+echo $existing_password;
+echo (crypt($password,$salt));
   // Check if the user input matches the stored information
   if ($username == $existing_username && password_verify($password, $existing_password)) {
     echo "Access granted!";

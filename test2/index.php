b4a7fdd4b403b77de84956dd3095f46b541984b9
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-02-22 18:46:10 +0000

    m

--- test2/index.php
+++ test2/index.php
@@ -83,6 +83,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     foreach ($_POST['line'] as $line) {
         $data .= $line . "\n";
     }
+    echo $data;
     $success = file_put_contents($file.date($timestamp), $data);
     if ($success === false) {
         echo 'Error saving changes to file.';

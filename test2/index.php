bd63c832985c9a65beb86ad00ec70b3ad21fb728
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-02-22 22:06:27 +0000

    m

--- test2/index.php
+++ test2/index.php
@@ -72,7 +72,7 @@ echo '<form method="post" enctype="multipart/form-data" action="' . htmlspecialc
 
 echo '<table width=60%>';
 foreach ($lines as $line_num => $line) {
-    echo '<tr><td contenteditable="true" style="text-align:left"><input type="text" name="line[]" value="' . htmlspecialchars($line) . '"></td></tr>';
+    echo '<tr><td contenteditable="true" style="text-align:left"><input width: 620px type="text" name="line[]" value="' . htmlspecialchars($line) . '"></td></tr>';
 
 }
 echo '</table>';

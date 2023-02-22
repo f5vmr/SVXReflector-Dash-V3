bfa4f2439815eec2e3f0fe854de03ed1933ca381
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-02-22 11:48:24 +0000

    m

--- editor.php
+++ editor.php
@@ -127,7 +127,7 @@ include_once __DIR__."/include/buttons.php";
     echo '</td></tr></table>';
 ?>
 
-<?php
+<?php/*
     echo '<div class="content2">'."\n";
     echo '<script type="text/javascript">'."\n";
     echo 'function reloadSysInfo(){'."\n";
@@ -140,7 +140,7 @@ include_once __DIR__."/include/buttons.php";
     include 'include/system.php';
     echo '</div>'."\n";
     echo '</div>'."\n";
-?>
+*/?>
 <?php
 if (MENUBUTTON=="BOTTOM") {
 include_once __DIR__."/include/buttons.php"; }

28fa89bb7a262850dddb379f1e70c31c314c24da
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-02-22 11:59:50 +0000

    m

--- editor.php
+++ editor.php
@@ -79,7 +79,30 @@ if (MENUBUTTON=="TOP") {
 include_once __DIR__."/include/buttons.php"; 
 }
 ?>
-<?php /*
+
+<?php 
+    echo '<table style="margin-bottom:0px;border:0; border-collapse:collapse; cellspacing:0; cellpadding:0; background-color:#f1f1f1;"><tr style="border:none;background-color:#f1f1f1;">';
+    echo '<td width="200px" valign="top" class="hide" style="height:auto;border:0;background-color:#f1f1f1;">';
+    echo '<div class="nav" style="margin-bottom:1px;margin-top:10px;">'."\n";
+
+    echo '<script type="text/javascript">'."\n";
+    echo 'function reloadStatusInfo(){'."\n";
+    echo '$("#statusInfo").load("include/status.php",function(){ setTimeout(reloadStatusInfo,3000) });'."\n";
+    echo '}'."\n";
+    echo 'setTimeout(reloadStatusInfo,3000);'."\n";
+    echo '$(window).trigger(\'resize\');'."\n";
+    echo '</script>'."\n";
+    echo '<div id="EditInfo" style="margin-bottom:30px;">'."\n";
+    include 'include/editor.php';
+    echo '</div>'."\n";
+    echo '</div>'."\n";
+    echo '</td>'."\n";
+    echo '<td valign="top" style="height:auto;border:none;  background-color:#f1f1f1;">';
+    echo '<div class="content">'."\n";
+    echo '<script type="text/javascript">'."\n";
+    echo '$(window).trigger(\'resize\');'."\n";
+    echo '</script>'."\n";
+/*
     echo '<table style="margin-bottom:0px;border:0; border-collapse:collapse; cellspacing:0; cellpadding:0; background-color:#f1f1f1;"><tr style="border:none;background-color:#f1f1f1;">';
     echo '<td width="200px" valign="top" class="hide" style="height:auto;border:0;background-color:#f1f1f1;">';
     echo '<div class="nav" style="margin-bottom:1px;margin-top:10px;">'."\n";

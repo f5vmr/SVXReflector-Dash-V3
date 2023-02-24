0169dab3b0734d146b3d20153638d6bb6f6e2543
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-02-24 13:45:47 +0000

    m

--- edit/index.php
+++ edit/index.php
@@ -194,7 +194,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }   
         //exec('sudo chown -R www-data:root /etc/svxlink/');
 }
-echo "<meta http-equiv='refresh' content='0'>";
+//echo "<meta http-equiv='refresh' content='0'>";
 exit();
 //Header('Location: ' . htmlspecialchars($_SERVER['PHP_SELF']));
 //exit(); 

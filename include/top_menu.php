c2c6f3733f98c1ca0df9b275a136d3479691c53e
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-02-25 13:36:45 +0000

    m

--- include/top_menu.php
+++ include/top_menu.php
@@ -15,7 +15,7 @@
 	<a href="/editor.php?id=gpio" style="color: crimson;" id="gpio">GPIO</a> |
 	<a href="/editor.php?id=echolink" style="color: crimson;" id="echolink">EchoLink</a> |
 	<a href="/editor.php?id=metarinfo" style="color: crimson;" id="metarinfo">MetarInfo</a> |
-	<a href="/node_Info.php?id=node_Info" style="color: crimson;" id="node_Info">node_Info</a></p>
+	<a href="/nodeInfo.php?id=nodeInfo" style="color: crimson;" id="nodeInfo">NodeInfo</a></p>
 	
 </p>
 	 

6ec08ab7347bf5703437c761fc16139066ecd00a
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-02-25 14:27:13 +0000

    m

--- nodeInfo/index.php
+++ nodeInfo/index.php
@@ -70,45 +70,6 @@ textarea {
 
 <?php 
 
-function build_ini_string(array $a) {
-    $out = '';
-    $sectionless = '';
-    foreach($a as $rootkey => $rootvalue){
-        if(is_array($rootvalue)){
-            // find out if the root-level item is an indexed or associative array
-            $indexed_root = array_keys($rootvalue) == range(0, count($rootvalue) - 1);
-            // associative arrays at the root level have a section heading
-            if(!$indexed_root) $out .= PHP_EOL."[$rootkey]".PHP_EOL;
-            // loop through items under a section heading
-            foreach($rootvalue as $key => $value){
-                if(is_array($value)){
-                    // indexed arrays under a section heading will have their key omitted
-                    $indexed_item = array_keys($value) == range(0, count($value) - 1);
-                    foreach($value as $subkey=>$subvalue){
-                        // omit subkey for indexed arrays
-                        if($indexed_item) $subkey = "";
-                        // add this line under the section heading
-                        $out .= "{$key}[$subkey] = $subvalue" . PHP_EOL;
-                    }
-                }else{
-                    if($indexed_root){
-                        // root level indexed array becomes sectionless
-                        $sectionless .= "{$rootkey}[] = $value" . PHP_EOL;
-                    }else{
-                        // plain values within root level sections
-                        $out .= "$key = $value" . PHP_EOL;
-                    }
-                }
-            }
-
-        }else{
-            // root level sectionless values
-            $sectionless .= "$rootkey = $rootvalue" . PHP_EOL;
-        }
-    }
-    return $sectionless.$out;
-}
-
 $password = "www-data";
 $command = "echo '$password' | sudo -S chmod -R 777 /etc/svxlink/";
 exec($command);
@@ -171,7 +132,7 @@ if (isset($_POST['btnSave']))
 	$inType = $nodeInfo["Type"];$inEcholink = $nodeInfo["Echolink"]; $innodeLocation = $nodeInfo["nodeLocation"];
 	$inSysop = $nodeInfo["Sysop"];$inNetwork = $nodeInfo["Network"]; $inCTCSS = $nodeInfo["CTCSS"];
 	$inLinkedTo = $nodeInfo["LinkedTo"];$inDefaultTg = $nodeInfo["DefaultTg"];
-
+echo $inLocation . " " . $inLocator;
 ?>
 
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">

3bdc5c007199021b5997a933e74a4186a2be82ba
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-02-25 11:58:24 +0000

    m

--- nodeInfo/index.php
+++ nodeInfo/index.php
@@ -158,10 +158,11 @@ textarea {
 $nodeInfoFile=$_GET['file'];
 exec('sudo cp ' . $nodeInfoFile . ' ' .$nodeInfoFile .'.bak');
 
-$lines = file($file);
+$lines = file($nodeInfofile);
+echo "Here Now with " . $lines;
 echo '<form method="post" enctype="multipart/form-data" action="' . htmlspecialchars($_SERVER['PHP_SELF']) . '">';
 echo '<table width=60%>';
-echo "Here Now with ";
+
 if (fopen($nodeInfoFile,'r'))
   {
   $filedata = file_get_contents($nodeInfoFile);

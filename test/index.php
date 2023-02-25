85dbdad7e756e5ae29b256265c93cda75c46c248
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-02-25 14:41:35 +0000

    modified

--- test/index.php
+++ test/index.php
@@ -1,13 +1,111 @@
-<!--<fieldset style="border:#3083b8 2px groove;box-shadow:5px 5px 20px #999; background-color:#f1f1f1; width:555px;margin-top:15px;margin-left:0px;margin-right:5px;font-size:13px;border-top-left-radius: 10px; border-top-right-radius: 10px;border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;">
-<div style="padding:0px;width:550px;background-image: linear-gradient(to bottom, #e9e9e9 50%, #bcbaba 100%);border-radius: 10px;-moz-border-radius:10px;-webkit-border-radius:10px;border: 1px solid LightGrey;margin-left:0px; margin-right:0px;margin-top:4px;margin-bottom:0px;line-height:1.6;white-space:normal;">
--->
-<!--h1 id="edit_info" style="color:#00aee8;font: 18pt arial, sans-serif;font-weight:bold; text-shadow: 0.25px 0.25px gray;">Edit Configuration '. $_GET['file']'</h1-->
-<?php //echo '<h1 id="edit_info" style="color:#00aee8;font: 18pt arial, sans-serif;font-weight:bold; text-shadow: 0.25px 0.25px gray;">Edit Configuration ' . $_GET['file'] . '</h1>';?>
+<?php 
+function build_ini_string(array $a) {
+    $out = '';
+    $sectionless = '';
+    foreach($a as $rootkey => $rootvalue){
+        if(is_array($rootvalue)){
+            // find out if the root-level item is an indexed or associative array
+            $indexed_root = array_keys($rootvalue) == range(0, count($rootvalue) - 1);
+            // associative arrays at the root level have a section heading
+            if(!$indexed_root) $out .= PHP_EOL."[$rootkey]".PHP_EOL;
+            // loop through items under a section heading
+            foreach($rootvalue as $key => $value){
+                if(is_array($value)){
+                    // indexed arrays under a section heading will have their key omitted
+                    $indexed_item = array_keys($value) == range(0, count($value) - 1);
+                    foreach($value as $subkey=>$subvalue){
+                        // omit subkey for indexed arrays
+                        if($indexed_item) $subkey = "";
+                        // add this line under the section heading
+                        $out .= "{$key}[$subkey] = $subvalue" . PHP_EOL;
+                    }
+                }else{
+                    if($indexed_root){
+                        // root level indexed array becomes sectionless
+                        $sectionless .= "{$rootkey}[] = $value" . PHP_EOL;
+                    }else{
+                        // plain values within root level sections
+                        $out .= "$key = $value" . PHP_EOL;
+                    }
+                }
+            }
+
+        }else{
+            // root level sectionless values
+            $sectionless .= "$rootkey = $rootvalue" . PHP_EOL;
+        }
+    }
+    return $sectionless.$out;
+}
+
+
+$password = "www-data";
+$command = "echo '$password' | sudo -S chmod -R 777 /etc/svxlink/";
+exec($command);
+exec('sudo chown -R www-data:www-data /etc/svxlink/');
+exec('sudo chown -R www-data:www:data /var/www/html');
+exec('sudo cp /etc/svxlink/node_info.json /etc/svxlink/node_info.bak');
+$nodeInfoFile = '/etc/svxlink/node_info.json';  
+
+if (fopen($nodeInfoFile,'r'))
+{
+	$filedata = file_get_contents($nodeInfoFile);
+	$nodeInfo = json_decode($filedata,true);
+
+	echo (build_ini_string(array($nodeInfo)));
+	print_r($nodeInfo);
+};
+
+
+
+
+
+
+if (isset($_POST['btnSave']))
+    {
+	$nodeInfo["Location"] = $_POST['inLocation']; $nodeInfo["Locator"] = $_POST['inLocator'];$nodeInfo["SysOp"] = $_POST['inSysOp'];
+	$nodeInfo["LAT"] = $_POST['inLAT']; $nodeInfo["LONG"] = $_POST['inLONG'];$nodeInfo["RXFREQ"] = $_POST['inRXFREQ'];
+	$nodeInfo["TXFREQ"] = $_POST['inTXFREQ']; $nodeInfo["Website"] = $_POST['inWebsite'];$nodeInfo["Mode"] = $_POST['inMode'];
+	$nodeInfo["Type"] = $_POST['inType']; $nodeInfo["Echolink"] = $_POST['inEcholink'];$nodeInfo["nodeLocation"] = $_POST['innodeLocation'];
+	$nodeInfo["Sysop"] = $_POST['inSysop']; $nodeInfo["Network"] = $_POST['inNetwork'];$nodeInfo["CTCSS"] = $_POST['inCTCSS'];
+	$nodeInfo["LinkedTo"] = $_POST['inLinkedTo'];$nodeInfo["DefaultTg"] = $_POST['inDefaultTg'];
+
+	$jsonNodeInfo = json_encode($nodeInfo);
+	file_put_contents("/var/www/html/nodeInfo/node_info.json", $jsonNodeInfo ,FILE_USE_INCLUDE_PATH);
+
+        $retval = null;
+        $screen = null;
+
+
+	///file manipulation section
+		//archive the current config
+		exec('sudo cp /etc/svxlink/node_info.json /etc/svxlink/node_info.json.' .date("YmdThis") ,$screen,$retval);
+		//move generated file to current config
+		exec('sudo mv /var/www/html/nodeInfo/node_info.json /etc/svxlink/node_info.json', $screen, $retval);
+        	//Service SVXlink restart
+       		exec('sudo service svxlink restart 2>&1',$screen,$retval);
+            exec('sudo chown -R www-data:root /etc/svxlink/');
+           };
+
+//  	$svxconfig = parse_ini_file($svxConfigFile,true,INI_SCANNER_RAW);
+//        $inCallsign = $svxconfig['ReflectorLogic']['CALLSIGN'];
+
+
+	$inLocation = $nodeInfo["Location"];$inLocator = $nodeInfo["Locator"]; $inSysOp = $nodeInfo["SysOp"];
+	$inLAT = $nodeInfo["LAT"];$inLONG = $nodeInfo["LONG"]; $inRXFREQ = $nodeInfo["RXFREQ"];
+	$inTXFREQ = $nodeInfo["TXFREQ"];$inWebsite = $nodeInfo["Website"]; $inMode = $nodeInfo["Mode"];
+	$inType = $nodeInfo["Type"];$inEcholink = $nodeInfo["Echolink"]; $innodeLocation = $nodeInfo["nodeLocation"];
+	$inSysop = $nodeInfo["Sysop"];$inNetwork = $nodeInfo["Network"]; $inCTCSS = $nodeInfo["CTCSS"];
+	$inLinkedTo = $nodeInfo["LinkedTo"];$inDefaultTg = $nodeInfo["DefaultTg"];
+echo $inLocation . " " . $inLocator;
+?>
+
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
 <table>
         <tr>
-        <th width = "640px">Node Info Input</th>
-	<th width = "625px">Action</th>
+        <th width = "380px">Node Info Input</th>
+	<th width = "100px">Action</th>
         </tr>
 <tr>
 <TD>
@@ -34,10 +132,6 @@
         <input  type="text" name="inSysOp" style="width:98%" value="<?php echo $inSysOp;?>">
         </td></tr>
         <tr style="border: none;"> 
-        <td style="border: none;">Hidden</td>
-        <td style="border: none;">
-        <input  type="text" name="inhidden" style="width:98%" value="<?php echo $inLAT;?>">
-        </td></tr><tr style="border: none;"> 
         <td style="border: none;">Lat</td>
         <td style="border: none;">
         <input  type="text" name="inLAT" style="width:98%" value="<?php echo $inLAT;?>">
@@ -88,9 +182,9 @@
         <input  type="text" name="inSysop" style="width:98%" value="<?php echo $inSysop;?>">
         </td></tr>
         <tr style="border: none;"> 
-        <td style="border: none;">Compound</td>
+        <td style="border: none;">Network</td>
         <td style="border: none;">
-        <input  type="text" name="inCompound" style="width:98%" value="<?php echo $inCompound;?>">
+        <input  type="text" name="inNetwork" style="width:98%" value="<?php echo $inNetwork;?>">
         </td></tr>
         <tr style="border: none;"> 
         <td style="border: none;">LinkedTo</td>
@@ -102,6 +196,13 @@
         <td style="border: none;">
         <input  type="text" name="inCTCSS" style="width:98%" value="<?php echo $inCTCSS;?>">
         </td></tr>
+        </td></tr>
+        <tr style="border: none;"> 
+        <td style="border: none;">DefaultTG</td>
+        <td style="border: none;">
+        <input  type="text" name="inDefaultTg" style="width:98%" value="<?php echo $inDefaultTg;?>">
+        </td></tr>
+
     </table>
 </td>
 <td> 
@@ -109,85 +210,4 @@
 </td>
 </tr>
 </table>
-<?php
-include_once('include/functions.php');
-$password = "www-data";
-$command = "echo '$password' | sudo -S chmod -R 777 /etc/svxlink/";
-exec($command);
-exec('sudo chown -R www-data:www-data /etc/svxlink/');
-exec('sudo chown -R www-data:www:data /var/www/html');
-
-
-
-$nodeInfoFile="/etc/svxlink/node_info.json";
-
-
-if (fopen($nodeInfoFile,'r'))
-  {
-  $filedata = file_get_contents($nodeInfoFile);
-  print_r($filedata);
-  $nodeInfo = json_decode($filedata,true);
-  print_r($nodeInfo);
-  build_ini_string(array($nodeInfo));
-  //print_r($sectionless . $out);
-  };
-if (isset($_POST['btnSave']))
-    {
-        $retval = null;
-        $screen = null;
-	
-    $nodeInfo["node"] = $_POST['innode']; 
-    $nodeInfo["Location"] = $_POST['inLocation'];
-    $nodeInfo["hidden"] = $_POST['inhidden'];
-    $nodeInfo["SysOp"] = $_POST['inSysOp'];
-	  $nodeInfo["ToneToTalkGroup"] = $_POST['inToneToTG'];
-    $nodeInfo["LAT"] = $_POST['inLAT']; 
-    $nodeInfo["LONG"] = $_POST['inLONG'];
-    $nodeInfo["Locator"] = $_POST['inLocator'];
-    $nodeInfo["RXFREQ"] = $_POST['inRXFREQ'];
-	  $nodeInfo["TXFREQ"] = $_POST['inTXFREQ']; 
-    $nodeInfo["Website"] = $_POST['inWebsite'];
-    $nodeInfo["Mode"] = $_POST['inMode'];
-	  $nodeInfo["Type"] = $_POST['inType']; 
-    $nodeInfo["Echolink"] = $_POST['inEcholink'];
-    $nodeInfo["nodeLocation"] = $_POST['innodeLocation'];
-	  $nodeInfo["Compound"] = $_POST['inCompound'];
-    $nodeInfo["CTCSS"] = $_POST['inCTCSS'];
-	  $nodeInfo["LinkedTo"] = $_POST['inLinkedTo'];
-
-	  $jsonnodeInfo = json_encode($nodeInfo);
-	  file_put_contents("/var/www/html/nodeInfo/node_info.json", $jsonnodeInfo ,FILE_USE_INCLUDE_PATH);
-
-        $retval = null;
-        $screen = null;
-        exec('sudo cp /etc/svxlink/node_info.json /etc/svxlink/node_info.json.' .date("YmdThis"), $screen, $retval);
-        exec('sudo mv /var/www/html/nodeInfo/node_info.json /etc/svxlink/node_info.json', $screen, $retval);
-        exec('sudo service svxlink restart 2>&1',$screen,$retval);
-    };
-    $svxconfig = parse_ini_file($svxConfigFile,true,INI_SCANNER_RAW);
-    $inCallsign = $svxconfig['ReflectorLogic']['CALLSIGN'];
-    $inPassword = $svxconfig['ReflectorLogic']['AUTH_KEY'];
-	  $inLocation = $nodeInfo["nodeLocation"];
-    $inLocator = $nodeInfo["loc"]; 
-    $inSysOp = $nodeInfo["sysop"];
-	  $inLAT = $nodeInfo["lat"];
-    $inLONG = $nodeInfo["long"]; 
-    $inRXFREQ = $nodeInfo["freq"];
-	  $inTXFREQ = $nodeInfo["TXFREQ"];
-    $inWebsite = $nodeInfo["Website"]; 
-    $inMode = $nodeInfo["Mode"];
-	  $inType = $nodeInfo["Type"];
-    $inEcholink = $nodeInfo["Echolink"]; 
-    $innodeLocation = $nodeInfo["nodeLocation"];
-	  $inSysop = $nodeInfo["Sysop"]; 
-    $inCTCSS = $nodeInfo["CTCSS"];
-	  $inLinkedTo = $nodeInfo["LinkedTo"];
-    
-?>
-
-
-
-<!--</form>
-<p style="margin: 0 auto;"></p>
-<p style="margin-bottom:-2px;"></p>
-</fieldset>-->
+</form>
